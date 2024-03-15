e7048b500dce3a102a1c0fda9de2ab41f2ea08f0
Dhyani Desai <56058074+dhyanid13@users.noreply.github.com>
2024-03-15 21:27:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 )
@@ -9,40 +11,42 @@ func main() {
 	proxywasm.SetVMContext(&vmContext{})
 }
 
-type vmContext struct {
-	// Embed the default VM context here,
-	// so that you don't need to implement all the methods.
-	proxywasm.DefaultVMContext
-}
+type vmContext struct{}
 
-func (*vmContext) NewPluginContext(contextID uint32) proxywasm.PluginContext {
+func (vmContext) NewPluginContext(contextID uint32) proxywasm.PluginContext {
 	return &pluginContext{}
 }
 
-type pluginContext struct {
-	// Embed the default plugin context here,
-	// so that you don't need to implement all the methods.
-	proxywasm.DefaultPluginContext
-}
+type pluginContext struct{}
 
-func (*pluginContext) NewHttpContext(contextID uint32) proxywasm.HttpContext {
+func (pluginContext) NewHttpContext(contextID uint32) proxywasm.HttpContext {
 	return &httpLifecycle{}
 }
 
 type httpLifecycle struct {
-	// Embed the default http context here,
-	// so that you don't need to implement all the methods.
 	proxywasm.DefaultHttpContext
 }
 
-// Override the methods you're interested in, e.g., onRequestHeaders.
+// Called when HTTP request headers are received.
 func (ctx *httpLifecycle) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
-	// Implement your JWT validation logic here
-	// For demonstration, we're just logging an HTTP request header.
-	if headerValue, err := proxywasm.GetHttpRequestHeader("authorization"); err == nil {
-		proxywasm.LogInfo("Authorization header: " + headerValue)
+	// Try to get the Authorization header from the incoming HTTP request.
+	authHeader, err := proxywasm.GetHttpRequestHeader("Authorization")
+	if err != nil {
+		proxywasm.LogError("failed to get Authorization header: " + err.Error())
+		return types.ActionContinue
 	}
 
-	// Continue the filter chain execution.
+	// The Authorization header is expected to be in the format "Bearer <token>".
+	// We split the header on whitespace and extract the token part.
+	parts := strings.SplitN(authHeader, " ", 2)
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		proxywasm.LogError("Authorization header format is not 'Bearer <token>'")
+		return types.ActionContinue
+	}
+	jwtToken := parts[1]
+
+	// Log the extracted JWT token.
+	proxywasm.LogInfo("Extracted JWT token: " + jwtToken)
+
 	return types.ActionContinue
 }
